dee0a046a34e87e6ae6193bd72974dca7878c460
Ryann <89058380+Zelqy@users.noreply.github.com>
2025-12-19 10:01:26 +0800

    Update koneksi.php

--- Tugas_5/koneksi.php
+++ Tugas_5/koneksi.php
@@ -1,14 +1,9 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$pass = "root";
-$db   = "sika";
-$port = 3306; // kalau Herd pakai port 3307, ganti di sini
-
-$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
+$koneksi = mysqli_connect("localhost", "root", "ZelqyAdrian", "stilo");
 
 // Cek error
 if (mysqli_connect_errno()) {
     die("Koneksi MariaDB gagal: " . mysqli_connect_error());
 }
 ?>
+
