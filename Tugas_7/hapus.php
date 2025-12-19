352bc9609096468e23bc1e0dfcbf4e48066e1e46
Ryann <89058380+Zelqy@users.noreply.github.com>
2025-12-19 22:17:43 +0800

    Delete Tugas_7 directory

--- Tugas_7/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "koneksi.php";
-
-$nim = $_GET['nim'];
-
-mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE nim='$nim'");
-
-echo "<script>alert('Data dihapus'); window.location='index.php';</script>";
-?>
